234711dc7392c967839fad6ae0206d8c991bb885
LbsTempest <l1641101764@gmail.com>
2024-05-29 20:52:04 +0800

    change

--- train_test.py
+++ train_test.py
@@ -28,7 +28,7 @@ def train_encoder_model(model, train_dataloader, dev_dataloader, criterion, opti
         accuracy: float = evaluate_model(model, dev_dataloader, device)
         if accuracy > model_accuracy:
             model_accuracy = accuracy
-            torch.save(model.state_dict(), f"Epoch:{epoch}_best_encoder_model.pth")
+            torch.save(model.state_dict(), f"Epoch_{epoch}_best_encoder_model.pth")
         print(f"Best Accuracy: {model_accuracy:.4f}")
         with open("encoder_loss.txt", "a") as f:
             f.write(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}\n")
@@ -55,7 +55,7 @@ def train_decoder_model(model, train_dataloader, dev_dataloader, criterion, opti
             if i % 100 == 0:
                 print(f"Batch {i}/{len(train_dataloader)} Loss: {loss.item()}")
             with open("decoder_loss.txt", "a") as f:
-                f.write(f"Epoch {epoch+1}/{num_epochs}, Batch {i}/{len(train_dataloader)}, Loss: {loss.item()}\n")
+                f.write(f"Epoch:{epoch+1}/{num_epochs}, Batch {i}/{len(train_dataloader)}, Loss: {loss.item()}\n")
 
         print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
         accuracy: float = evaluate_model(model, dev_dataloader, device)
